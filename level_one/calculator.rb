fecbee66bed0545ad1d9359c5667db998a7c4214
Dev <dev@divethree.com>
2018-02-22 09:07:48 -0800

    fix.

--- level_one/calculator.rb
+++ level_one/calculator.rb
@@ -1,13 +1,39 @@
-# Create a method for each of the tasks below.
-# The methods should return the expected value
-
 # Add two numbers
-
+def add(num1, num2)
+  num1 + num2
+end
 
 # Subtract two numbers
+def subtract(num1, num2)
+  num1 - num2
+end
 
 # Add an array of numbers
+def sum(arr)
+  # The Ruby way...
+  arr.reduce { |sum, n| sum + n } # A.K.A arr.inject(:+)
+
+  # Another way ...
+  # result = 0
+  # i = 0
+  # while i < arr.length
+  #   result += arr[i]
+  #   i += 1
+  # end
+  # result
+end
 
 # Multiply an array of numbers
+def multiply(nums)
+  # The Ruby way ...
+  nums.reduce { |product, n| product * n } # A.K.A nums.inject(:*)
 
-# A method that takes two numbers and creates an exponent where num1 is raised to the power of num2
+  # Another way ...
+  # product = 1
+  # nums.each { |x| product *= x}
+  # product
+end
+
+def power(num1, num2)
+  num1**num2
+end
